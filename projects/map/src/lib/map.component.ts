3932c66be56980af19d6f0d5244a870d13b4eaa8
zhangjing <892295834@qq.com>
2025-09-15 16:21:22 +0800

    skeleton commit

---
+++ projects/map/src/lib/map.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-map',
+  imports: [],
+  template: `
+    <p>
+      map works!
+    </p>
+  `,
+  styles: ``
+})
+export class MapComponent {
+
+}
---
+++ projects/map/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of map
+ */
+
+export * from './lib/map.service';
+export * from './lib/map.component';
